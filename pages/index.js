ad82a19a8eb1d49bf32c91c55c3605dc54ec9226
Talys Eugênio <talysebf@gmail.com>
2022-01-25 23:37:51 -0300

    second class

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import appConfig from "../config.json"
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-
-function GlobalStyle(){
-  return(
-    <style global jsx>{`
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      list-style: none;
-    }
-    body {
-      font-family: 'Open Sans', sans-serif;
-    }
-    /* App fit Height */ 
-    html, body, #__next {
-      min-height: 100vh;
-      display: flex;
-      flex: 1;
-    }
-    #__next {
-      flex: 1;
-    }
-    #__next > * {
-      flex: 1;
-    }
-    /* ./App fit Height */ 
-  `}</style>
-);
-}
+import React from "react";
+import {useRouter} from "next/router"
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -61,15 +34,16 @@ function Titulo(props) {
 //   export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'talyseugenio';
+//const username = 'talyseugenio';
+  const [username, setUsername] =  React.useState('talyseugenio')
+  const roteamento = useRouter()
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundColor: "#DCDCDC",
           backgroundImage: 'url(https://files.tecnoblog.net/wp-content/uploads/2019/07/gta-san-andreas-001-1-700x475.jpg)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
@@ -92,6 +66,12 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(event) =>{
+              event.preventDefault()
+              console.log("Alguem submeteu o form")
+              roteamento.push("/chat")
+              //window.location.href = "/chat"
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -101,14 +81,34 @@ export default function PaginaInicial() {
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
               {appConfig.name}
             </Text>
+            {/* <input type="text"
+            value = {username}
+            onChange = {(event) =>{
+              console.log("O usuário digitou!", event.target.value)
+              //Trocar o valor da variável
+              const valor = event.target.value
+              //trocar o valor da variável
+              setUsername(valor);
+            }}
+            /> */}
 
             <TextField
+            placeholder="Informe usuário GitHub"
+             value = {username}
+             onChange = {(event) =>{
+               console.log("O usuário digitou!", event.target.value)
+               //Trocar o valor da variável
+               const valor = event.target.value
+               //trocar o valor da variável
+               setUsername(valor);
+             }}
+             
               fullWidth
               textFieldColors={{
                 neutral: {
                   textColor: appConfig.theme.colors.neutrals[200],
                   mainColor: appConfig.theme.colors.neutrals[900],
-                  mainColorHighlight: appConfig.theme.colors.primary[500],
+                  mainColorHighlight: "#DCDCDC",
                   backgroundColor: appConfig.theme.colors.neutrals[800],
                 },
               }}
@@ -116,12 +116,13 @@ export default function PaginaInicial() {
             <Button
               type='submit'
               label='Entrar'
+              disabled = {username < 3}
               fullWidth
               buttonColors={{
                 contrastColor: appConfig.theme.colors.neutrals["000"],
-                mainColor: appConfig.theme.colors.primary[500],
-                mainColorLight: appConfig.theme.colors.primary[400],
-                mainColorStrong: appConfig.theme.colors.primary[600],
+                mainColor: "#696969",
+                mainColorLight: "#363636",
+                mainColorStrong: "#363636",
               }}
             />
           </Box>
